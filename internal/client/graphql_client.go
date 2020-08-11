82f98e8c7755a62c8d200f556aea6b49fd1ce0e0
schettn <nicoschett@icloud.com>
2020-08-12 00:21:27 +0200

    Introduce ops integration
    Now the pipeline data is published to  the
    given ops endpoint.
    The snek cisco user in combination with a unique
    pipeline token(OPS_TOKEN) is used for authentication.

--- internal/client/graphql_client.go
+++ internal/client/graphql_client.go
@@ -3,8 +3,6 @@ package client
 import (
 	"context"
 	"errors"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 
@@ -21,10 +19,6 @@ func getEnv(key string) string {
 
 // SendToOPS send a datastring to the OPS SITP endpoint
 func SendToOPS(data string) {
-	fmt.Println("GraphQL Client Received Data ")
-	ioutil.WriteFile("tmp.json", []byte(data), 0644)
-	fmt.Print(data)
-
 	// assign env vars
 	OPSURL := getEnv("OPS_URL")
 	OPSTOKEN := getEnv("OPS_TOKEN")
@@ -32,33 +26,62 @@ func SendToOPS(data string) {
 	// create a client (safe to share across requests)
 	client := graphql.NewClient(OPSURL)
 
-	// make a request
-	req := graphql.NewRequest(`
-    query ($token: String!, $key: String!) {
-        items (id:$key) {
-            field1
-            field2
-            field3
-        }
-    }
-	`)
-
-	// set any variables
-	req.Var("token", OPSTOKEN)
-
-	// set header fields
-	req.Header.Set("Authorization", OPSTOKEN)
-
 	// define a Context for the request
 	// Ref: https://golang.org/pkg/context/
 	ctx := context.Background()
 
+	req := graphql.NewRequest(`
+		mutation tokenAuth($username: String!, $password: String!) {
+				tokenAuth(username: $username, password: $password) {
+					token
+				}
+			}
+	`)
+
+	req.Var("username", "cisco")
+	req.Var("password", "ciscocisco")
+
 	// run it and capture the response
-	var respData ResponseStruct
+	var respData map[string]map[string]interface{}
+	client.Run(ctx, req, &respData)
 	if err := client.Run(ctx, req, &respData); err != nil {
-		log.Fatal(err)
+		// log.Fatal(err)
+	}
+
+	JWT := respData["tokenAuth"]["token"]
+
+	// make a request
+	req = graphql.NewRequest(`
+		mutation publishPipelineData(
+			$token: String!
+			$url: String!
+			$values: GenericScalar!
+		) {
+			opsPipelineFormPage(token: $token, url: $url, values: $values) {
+				result
+			}
+		}
+	`)
+
+	type valuesStruct struct {
+		PipelineToken string `json:"pipeline_token"`
+		RawData       string `json:"raw_data"`
+	}
+
+	values := valuesStruct{OPSTOKEN, data}
+
+	// set any variables
+	req.Var("token", JWT)
+	req.Var("url", "/pipeline/")
+	req.Var("values", values)
+
+	// run it and capture the response
+	client.Run(ctx, req, &respData)
+	if err := client.Run(ctx, req, &respData); err != nil {
+		// log.Fatal(err)
 	}
 }
 
 // ResponseStruct not defined yet
+// map[opsPipelineFormPage:map[result:FAIL]]
 type ResponseStruct struct{}
